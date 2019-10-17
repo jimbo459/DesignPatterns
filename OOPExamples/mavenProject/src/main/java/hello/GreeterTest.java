736033204cf47078ae600fe2ff07626638c284e3
James Norman <jnorman@pivotal.io>
2019-10-17 18:22:01 +0100

    add maven project

---
+++ OOPExamples/mavenProject/src/main/java/hello/GreeterTest.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.hamcrest.CoreMatchers.containsString;
+import org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreeterTest {
+
+	private Greeter greeter = new Greeter();
+
+	@Test
+	public void greeterSaysHello() {
+		assertThat(greeter.sayHello(), containsString("Hello"));
+	}
+
+}
